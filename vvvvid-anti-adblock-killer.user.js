87dac4aa9ea67298fad6d79659c76ccbf3eb9f5b
Nearata <nearata@protonmail.com>
2018-10-27 21:06:05 +0200

    Updated version

--- vvvvid-anti-adblock-killer.user.js
+++ vvvvid-anti-adblock-killer.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/Nearata/vvvvid-anti-adblock-killer
 // @copyright   2018+, William Di Cicco (Nearata)
 // @license     MIT
-// @version     1.0
+// @version     1.0.1
 // @description Previene la disattivazione dell'adblock
 // @author      Nearata <nearata@protonmail.com>
 // @supportURL  https://github.com/Nearata/vvvvid-anti-adblock-killer/issues
