655974edef4c45a4f92ecabd11937ea6ac4607e1
Nearata <nearata@protonmail.com>
2018-10-28 15:54:58 +0100

    Cleanup

--- vvvvid-anti-adblock-killer.user.js
+++ vvvvid-anti-adblock-killer.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/Nearata/vvvvid-anti-adblock-killer
 // @copyright   2018+, William Di Cicco (Nearata)
 // @license     MIT
-// @version     1.0.2
+// @version     1.0.3
 // @description Previene la disattivazione dell'adblock
 // @author      Nearata <nearata@protonmail.com>
 // @supportURL  https://github.com/Nearata/vvvvid-anti-adblock-killer/issues
@@ -16,19 +16,8 @@
     'use strict';
 
     function startAdv(g, c, f) {
-        const e = this;
-        const d = e.playlistItem.toJSON();
-
-        if(("vod_mode" in d) && (d.vod_mode & 1) === 0 && !window.vvvvid.user.svodActive()) {
-            window.vvvvid.showGenericError("Il video selezionato è disponibile solo in modalità abbonamento.", function() {
-                window.vvvvid.router.mainView.resetApplication();
-            });
-            return;
-        }
-
-        if(!g) {
-            e.destroyPlayers();
-        }
+        g = null;
+        c = null;
 
         f();
     }
